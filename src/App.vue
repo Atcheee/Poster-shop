7694b54781ab8906e8d4a1fbe2fb0d5ad8821b4e
Atche <erik90781@gmail.com>
2022-02-10 11:30:11 +0100

    Test x2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">1</router-link> |
       <router-link to="/about">shöoueghsöouege</router-link>
     </div>
     <router-view/>
