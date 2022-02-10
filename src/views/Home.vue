5c7b1e973e4ed0b8102efdc906e45faf3482739d
Atche <erik90781@gmail.com>
2022-02-10 14:01:52 +0100

    Add json data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/char-1.png">
     <h1>Poster Shop</h1>
     <p>Buy posters here</p>
   </div>
 </template>
+
+<style>
+* {
+  background-color: black;
+}
+</style>
