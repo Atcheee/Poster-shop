9ae30ba21ba1f86a240087fb7fb9eb93e42bd199
Tejaswi Vutukuri <tejaswi.vutukuri@iths.com>
2022-02-10 11:19:08 +0100

    Test Developer Branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <h1>Poster Shop Home Page</h1>
   </div>
 </template>
