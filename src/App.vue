e3cafbe2d993308fe413f37fb4fe6e037d0c1265
Atche <erik90781@gmail.com>
2022-02-10 11:51:05 +0100

    Small Test Change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">1</router-link> |
-      <router-link to="/about">shöoueghsöouege</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
