13b25857bab95ede7b4baa590156ea51703693a3
Atche <erik90781@gmail.com>
2022-02-10 16:01:49 +0100

    Fix code confliction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <Header />
-  <!--   <img alt="Vue logo" src="../assets/char-1.png"> -->
+    <!--   <img alt="Vue logo" src="../assets/char-1.png"> -->
     <h1>Poster Shop</h1>
     <p>Buy posters here</p>
     <router-link
@@ -9,15 +9,16 @@
       v-for="poster in posters"
       :key="poster.id"
     >
-    <img :src="poster.img" alt="Poster">
-    <h1>{{ poster.job }}</h1>
+      <img :src="poster.img" alt="Poster" />
+      <h1>{{ poster.job }}</h1>
     </router-link>
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/Header.vue";
 export default {
+  components: { Header },
   computed: {
     posters() {
       return this.$store.state.Posters;
@@ -30,22 +31,9 @@ export default {
 * {
   color: white;
   text-decoration: none;
+  margin: 0;
 }
 img {
   width: 100%;
-=======
-import Header from '@/components/Header.vue'
-
-export default {
-  components: {Header}
 }
-</script>
-
-<style>
-*{
-  margin: 0;
-}
-/* * {
-  background-color: black;
-} */
 </style>
